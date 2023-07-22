45d03255b6c8b41deaf0413aaab2d48fb5cf2d85
Snuffish <snuffish90@gmail.com>
2023-07-22 10:06:12 +0200

    wip

--- src/m/test.m
+++ src/m/test.m
@@ -1,22 +1,38 @@
 Test
-
-	S *FILE=$$READFILE^%FS("files/package.yaml")
-
-	S INDENT=$C(32,32)
-
-	S ROW=$NA(FILE) FOR  S ROW=$Q(@ROW) Q:(ROW="")  DO
-	.S LINE=$QS(ROW,1)
-	.S VALUE=@ROW
-	.W "["_LINE_"] "_VALUE,!
-
+	N
 
 	W !!!
 
+	S *FILE=$$READFILE^%FS("files/package.yaml")
+
+	S INDENTCHAR=32
+
+	S APPNAME=""
+
+	S ROW=$NA(FILE) FOR  S ROW=$Q(@ROW) Q:(ROW="")  DO
+	.S NR=$QS(ROW,1)
+	.S LINE=@ROW
+	.;; Get the AppName and the Start-point in the YAML file.
+	.IF $C($E(LINE,1))'=INDENTCHAR  DO
+	..S:(APPNAME="") APPNAME=$TR(LINE,":")
+	.;;
+	.IF $$ISNEWBLOCK(LINE)  DO
+	..W "NEW BLOCK => "_$TR(LINE,$C(INDENTCHAR)),!
+
+	W APPNAME
+
 	Q
 
+BLOCK()
+
+	Q
+
+ISNEWBLOCK(LINE)
+	Q $A($E(LINE,1))=32
 
 
-
+;.IF $E($RE(VALUE))=":" W "END OF BLOCK:"_VALUE,!
+;.W "["_LINE_"] "_VALUE,!
 
 
 
